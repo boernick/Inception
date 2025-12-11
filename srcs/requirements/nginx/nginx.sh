e5170c6723bc0693b65fdf871115ae84affa7a3c
Nick Boer <nboer@c3a3c7.42berlin.de>
2025-12-11 16:01:46 +0100

    update

--- srcs/requirements/nginx/nginx.sh
+++ srcs/requirements/nginx/nginx.sh
@@ -1,6 +1,7 @@
 chown -R www-data:www-data /var/log/nginx/nginx
 chown -R www-data:www-data /var/www/html
 mkdir -p /var/www/html/static/
-cp /etc/nginx/index.html /var/www/html/static/index.html
+mkdir -p /var/www/html/wordpress/
+# cp /etc/nginx/index.html /var/www/html/static/index.html
 cp /run/secrets/ssl_private_key /etc/ssl/private/nginx-selfsigned.key
 nginx -g "daemon off;"
